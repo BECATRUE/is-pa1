32efe64f943744107d85cde4fc611df148904b7e
BECATRUE <becatrueyjh@gmail.com>
2022-10-11 00:20:20 +0900

    refactor: Remove pointer
    Remove pointer of all mpz variables.
    
    See also: #2

--- keygen.c
+++ keygen.c
@@ -7,26 +7,26 @@
 // Parameter: 
 //  str_p: string of first number
 //  str_q: string of second number
-//  p    : address of first number
-//  q    : address of second number
+//  p    : first number
+//  q    : second number
 //
 // Return:
 //   0: valid
 //  -1: invalid
-int validateInput(char *str_p, char *str_q, mpz_t *p, mpz_t *q) {
+int validateInput(char *str_p, char *str_q, mpz_t p, mpz_t q) {
     // 1. Validate whether two inputs are integers
-    if (mpz_init_set_str(*p, str_p, 10) == -1) {
+    if (mpz_init_set_str(p, str_p, 10) == -1) {
         printf("The first input is not an integer.\n");
         return -1;
     }
 
-    if (mpz_init_set_str(*q, str_q, 10) == -1) {
+    if (mpz_init_set_str(q, str_q, 10) == -1) {
         printf("The second input is not an integer.\n");
         return -1;
     }
 
     // 2. Validate whether two inputs are prime
-    int isPrime = mpz_probab_prime_p(*p, 50);
+    int isPrime = mpz_probab_prime_p(p, 50);
     if (isPrime == 0) {
         printf("The first input is not prime.\n");
         return -1;
@@ -35,7 +35,7 @@ int validateInput(char *str_p, char *str_q, mpz_t *p, mpz_t *q) {
         printf("Not sure, but proceed.\n");
     }
 
-    isPrime = mpz_probab_prime_p(*q, 50);
+    isPrime = mpz_probab_prime_p(q, 50);
     if (isPrime == 0) {
         printf("The second input is not prime.\n");
         return -1;
@@ -48,6 +48,33 @@ int validateInput(char *str_p, char *str_q, mpz_t *p, mpz_t *q) {
 }
 
 
+// Function computing n
+//
+// Parameter: 
+//  n: result
+//  p: first number
+//  q: second number
+void computeN(mpz_t n, mpz_t p, mpz_t q) {
+    mpz_mul(n, p, q);
+}
+
+
+// Function computing phi
+//
+// Parameter: 
+//  phi: result
+//  p  : first number
+//  q  : second number
+void computePhi(mpz_t phi, mpz_t p, mpz_t q) {
+    mpz_t p_1, q_1;
+
+    mpz_sub_ui(p_1, p, 1);
+    mpz_sub_ui(q_1, q, 1);
+
+    mpz_mul(phi, p_1, q_1);
+}
+
+
 // Function generating key
 //
 // Parameter:
@@ -55,11 +82,16 @@ int validateInput(char *str_p, char *str_q, mpz_t *p, mpz_t *q) {
 //  str_q: string of second number
 void generateKey(char *str_p, char *str_q) {
     mpz_t p, q;
-    mpz_t n;
+    mpz_t n, phi;
 
     // 1. Validate whether two inputs are valid
-    if (validateInput(str_p, str_q, &p, &q) == -1) { return; }
+    if (validateInput(str_p, str_q, p, q) == -1) { return; }
 
     // 2. Compute n = pq
-    mpz_mul(n, p, q);
+    computeN(n, p, q);
+    
+    // 3. Compute phi = (p - 1)(q - 1)
+    computePhi(phi, p, q);
+
+    gmp_printf("%Zd\n", phi);
 }
