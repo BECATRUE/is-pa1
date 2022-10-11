4cad124db98ea6eab3ade85c1389a5f75de8d79c
BECATRUE <becatrueyjh@gmail.com>
2022-10-12 04:51:03 +0900

    feat: Implement reading plaintext
    Implement reading plaintext.
    - Add textread.c
    
    See also: #3

--- encrypt.c
+++ encrypt.c
@@ -4,12 +4,21 @@
 
 
 extern int readPublicKey(mpz_t e, mpz_t n);
+extern int readPlainText(mpz_t pt);
 
 
 // Encrypting function
 void encrypt() {
     mpz_t e, n;
+    mpz_t pt;
+
+    mpz_inits(e, n, pt, NULL);
 
     // 1. Read public key
     if (readPublicKey(e, n) == -1) { return; }
+
+    // 2. Read plaintext
+    if (readPlainText(pt) == -1) { return; }
+
+    mpz_clears(e, n, pt, NULL);
 }
