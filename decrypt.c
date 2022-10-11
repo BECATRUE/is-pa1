b8cc5aaba376f013de786bc0395710bf35a105d3
BECATRUE <becatrueyjh@gmail.com>
2022-10-12 05:30:29 +0900

    feat: Implement reading keys
    Implement reading keys.
    
    See also: #4

--- decrypt.c
+++ decrypt.c
@@ -1,4 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <gmp.h>
+
+
+extern int readPublicKey(mpz_t e, mpz_t n);
+extern int readPrivateKey(mpz_t d);
+
+
 // Decrypting function
 void decrypt() {
-    
+    mpz_t e, n, d;
+
+    mpz_inits(e, n, d, NULL);
+
+    // 1. Read public key
+    if (readPublicKey(e, n) == -1) { return; }
+
+    // 2. Read private key
+    if (readPrivateKey(d) == -1) { return; }
+
+    mpz_clears(e, n, d, NULL);
 }
