31d972a99222c8b950ca333cea6676881daa8e16
BECATRUE <becatrueyjh@gmail.com>
2022-10-12 05:33:28 +0900

    feat: Implement function reading ciphertext
    Implement function reading ciphertext.
    
    See also: #4

--- textread.c
+++ textread.c
@@ -50,3 +50,52 @@ int readPlainText(mpz_t pt) {
 
     return 0;
 }
+
+
+// Function reading ciphertext
+//
+// Parameter:
+//  ct: ciphertext
+//
+// Return:
+//   0: success to read
+//  -1: fail to read
+int readCipherText(mpz_t ct) {
+    char str[1024];
+    FILE *ciphertext_file;
+
+    // Open ciphertext file
+    ciphertext_file = fopen("./text/ciphertext", "rt");
+
+    if (ciphertext_file == NULL) {
+        printf("There is no ciphertext file.\n");
+        printf("The ciphertext must be stored in ./text/ciphertext.\n");
+        return -1;
+    }
+
+    fseek(ciphertext_file, 0, SEEK_END);
+    if (ftell(ciphertext_file) >= 1024) {
+        printf("The ciphertext is too long.\n");
+        printf("The length of ciphertext must be at most 1023.\n");
+        return -1;
+    }
+    fseek(ciphertext_file, 0, SEEK_SET);
+
+    // Read ciphertext
+    fgets(str, 10, ciphertext_file);
+
+    if (mpz_init_set_str(pt, str, 10) == -1) {
+        printf("The ciphertext is invalid.\n");
+        return -1;
+    }
+
+    if (mpz_sgn(pt) <= 0) {
+        printf("The ciphertext must be positive.\n");
+        return -1;
+    }
+
+    // Close ciphertext file
+    fclose(ciphertext_file);
+
+    return 0;
+}
