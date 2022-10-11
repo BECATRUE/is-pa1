394bb78f6e854e5aebf2e96cd8f4fe89d8700b45
BECATRUE <becatrueyjh@gmail.com>
2022-10-12 04:54:24 +0900

    feat: Define function computing ciphertext
    Define function computing ciphertext.
    
    See also: #3

--- encrypt.c
+++ encrypt.c
@@ -7,18 +7,34 @@ extern int readPublicKey(mpz_t e, mpz_t n);
 extern int readPlainText(mpz_t pt);
 
 
+// Compute ciphertext
+//
+// Parameter:
+//  pt: plaintext
+//  ct: ciphertext
+//  e
+//  n
+void computeCiphertext(mpz_t pt, mpz_t ct, mpz_t e, mpz_t n) {
+
+}
+
+
+
 // Encrypting function
 void encrypt() {
     mpz_t e, n;
-    mpz_t pt;
+    mpz_t pt, ct;
 
-    mpz_inits(e, n, pt, NULL);
+    mpz_inits(e, n, pt, ct, NULL);
 
     // 1. Read public key
     if (readPublicKey(e, n) == -1) { return; }
 
     // 2. Read plaintext
     if (readPlainText(pt) == -1) { return; }
+
+    // 3. Compute ciphertext
+    computeCiphertext(pt, ct, e, n);
 
     mpz_clears(e, n, pt, NULL);
 }
