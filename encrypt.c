73fd03edec4cf2082238f5aa8cf77d97a0f92bd0
BECATRUE <becatrueyjh@gmail.com>
2022-10-12 05:13:07 +0900

    feat: Implement saving ciphertext
    Implement saving ciphertext.
    Print descriptions.
    
    Close: #3

--- encrypt.c
+++ encrypt.c
@@ -7,7 +7,7 @@ extern int readPublicKey(mpz_t e, mpz_t n);
 extern int readPlainText(mpz_t pt);
 
 
-// Compute ciphertext
+// Function computing ciphertext
 //
 // Parameter:
 //  pt: plaintext
@@ -18,6 +18,29 @@ void computeCiphertext(mpz_t pt, mpz_t ct, mpz_t e, mpz_t n) {
     mpz_powm(ct, pt, e, n);
 }
 
+
+// Function saving ciphertext
+//
+// Parameter:
+//  pt: plaintext
+//  ct: ciphertext
+void saveCiphertext(mpz_t pt, mpz_t ct) {
+    FILE *ciphertext_file;
+
+    // Save ciphertext
+    ciphertext_file = fopen("./text/ciphertext", "wt");
+    gmp_fprintf(ciphertext_file, "%Zd", ct);
+    fclose(ciphertext_file);
+
+    // Print descriptions
+    printf("Plaintext\n");
+    gmp_printf("   : %Zd\n\n", pt);
+
+    printf("Encrypted ciphertext\n");
+    gmp_printf("   : %Zd\n\n", ct);
+
+    printf("The encrypted ciphertext is stored in ./text/ciphertext.\n\n");
+}
 
 
 // Encrypting function
@@ -35,7 +58,9 @@ void encrypt() {
 
     // 3. Compute ciphertext
     computeCiphertext(pt, ct, e, n);
-    gmp_printf("%Zd\n", ct);
 
-    mpz_clears(e, n, pt, NULL);
+    // 4. Save ciphertext
+    saveCiphertext(pt, ct);
+
+    mpz_clears(e, n, pt, ct, NULL);
 }
